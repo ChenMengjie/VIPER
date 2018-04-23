0cdac1daa4f7dd5dda81d64ef1aedd5a6300f37a
ChenMengjie <mengjiechen@uchicago.edu>
2018-04-22 23:07:43 -0500

    Fix output bugs

--- R/imputation_model.R
+++ R/imputation_model.R
@@ -61,11 +61,11 @@ VIPER <- function(gene.expression, num = 5000, percentage.cutoff = 0.1, minbool 
   rownames(imputed_counts) <- rownames(nopredict) <- rownames(xx)
 
   res <- list(imputed = imputed_counts, imputed_log = nopredict,
-              sample_weights = res_imp$sample_weights, outliers=outliers)
-  writel.table(imputed_counts, file = paste0(outdir, "/", prefix, "imputed_counts.csv"))
-  writel.table(res$imputed_log, file = paste0(outdir, "/", prefix, "imputed_logvalue.csv"))
-  writel.table(res$sample_weights, file = paste0(outdir, "/", prefix, "sample_weights.csv"))
-  writel.table(res$outliers, file = paste0(outdir, "/", prefix, "outliers.csv"))
+              sample_weights = res_imp$sample_weights, outliers = outliers)
+  write.table(imputed_counts, file = paste0(outdir, "/", prefix, "imputed_counts.csv"))
+  write.table(res$imputed_log, file = paste0(outdir, "/", prefix, "imputed_logvalue.csv"))
+  write.table(res$sample_weights, file = paste0(outdir, "/", prefix, "sample_weights.csv"))
+  write.table(res$outliers, file = paste0(outdir, "/", prefix, "outliers.csv"))
 
   return(res)
 }
